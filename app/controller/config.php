e642c6baf07b01e7374772f958fe30a27c01a7b8
SylvanoP <47647452+SylvanoP@users.noreply.github.com>
2020-02-20 04:17:24 +0100

    Create config.php

---
+++ app/controller/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$db_host = 'localhost';
+$db_name = 'name';
+$db_username = 'username';
+$db_password = 'password';
+
+$siteName = 'Paste Service';
+
+$url = 'https://paste.plocic.de/';
+$cdnUrl = $url.'assets/style/';
+$picUrl = $url.'assets/images/';
